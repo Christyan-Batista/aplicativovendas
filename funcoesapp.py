5dd11a9c2973df7f674ac034b3bf12f7a0255a52
João Pedro <joao.psb20@gmail.com>
2022-10-09 23:21:09 -0300

    Adicionado função de cadastrar produtos

--- funcoesapp.py
+++ funcoesapp.py
@@ -3,23 +3,30 @@ import pandas as pd
 class iniciarfuncoes():
 
     def __init__(self):
-        self.adress = "dados.xlsx"
+        self.address = 'dados.xlsx'
 
 
     def cadastrarusuario(self, nome, email, telefone, cpf, endereco):
-        self.nome = name
+        self.nome = nome
         self.email = email
         self.telefone = telefone
         self.cpf = cpf
         self.endereco = endereco
-        df = pd.DataFrame({"Nome": [self.nome], 
+        df_usuario = pd.DataFrame({"Nome": [self.nome], 
                            "Email":[self.email],
                            "Telefone": [self.telefone],
                            "Cpf": [self.cpf],
-                           "Endereco":[self.endereco]})
-        df_excel = pd.read_excel(self.adress, sheet_name="CadastroUsuario")
-        adicionarcadastro = pd.concat([df_excel, df], ignore_index=True)
-        adicionarcadastro.to_excel(self.adress,sheet_name='CadastroUsuario', index=False)
+                           "Endereco":[self.endereco]})  #Tranformando os dados para um formato legível no excel
 
-        print(df)
-        return
+
+    def cadastroprodutos(self, categoria, produto, cor, precocompra, precovenda):
+        self.categoria = categoria
+        self.produto = produto
+        self.cor = cor
+        self.precocompra = precocompra
+        self.precovenda = precovenda
+        df_produto = pd.DataFrame({"categoria": [self.categoria],
+                                   "produto": [self.produto],
+                                   "cor": [self.cor],
+                                   "precocompra": [self.precocompra],
+                                   "precovenda": [self.precovenda]})  #Tranformando os dados para um formato legível no excel
