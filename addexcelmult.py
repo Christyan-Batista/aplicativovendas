5ea83464b03de2ab651d4789ae46b9c1344999a1
Christyan Batista <67526341+Christyan-Batista@users.noreply.github.com>
2022-10-12 19:14:05 -0300

    Update addexcelmult.py

--- addexcelmult.py
+++ addexcelmult.py
@@ -11,9 +11,26 @@ def addusuarioexcel(df, df_excel):  #Função que insere uma nova linha na plani
 
 
 def addprodutoexcel(df_prod, df_excel):  #Função que insere uma nova linha na planilha com o novo produto
-    resultado_concat = pd.concat([df_excel, df_prod])
+    resultado_conc = pd.concat([df_excel, df_prod])
     with pd.ExcelWriter(arquivo_xml, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
-        resultado_concat.to_excel(writer, sheet_name='CadastroProduto', index=False)
+        resultado_conc.to_excel(writer, sheet_name='CadastroProduto', index=False)
     return
+
+
+def cadastrarvenda(df_venda, df_excel_venda):
+    resultado_concat = pd.concat([df_excel_venda, df_venda])
+    with pd.ExcelWriter(arquivo_xml, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        resultado_concat.to_excel(writer, sheet_name='Vendas', index=False) #Adicionando a venda a folha de venda
+    return
+
+
+def lerplanilha(stringplanilha):  #Funcão que lê a planilha e retorna para determinado fim
+    ler = pd.read_excel(arquivo_xml, sheet_name=stringplanilha)
+    return ler
+
+
+def gerarcodigoproduto():
+    print()
+    print()
 
 
