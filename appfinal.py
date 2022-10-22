37394d15649fbc853150a0e7eb69de69f176431e
Christyan-Batista <chris.batissta@gmail.com>
2022-10-22 17:50:18 -0300

    incluido funcões de verificar existência do usuario

--- appfinal.py
+++ appfinal.py
@@ -2,12 +2,10 @@ import funcoesapp
 
 iniciar = funcoesapp.iniciarfuncoes()
 
-categoria = str(input('Digite o nome:'))
-produto = str(input('Digite o produto:'))
-cor = str(input('Digite a cor:'))
-precocompra = float(input('Digite o preço de compra:'))
-precovenda = float(input('Digite o preço de venda:'))
-estoque = int(input('Digite a quantidade comprada:'))
+usuario = str(input('Nome:'))
+cpf = str(input('Cpf:'))
+email = str(input('Email:'))
+tel = str(input('Telefone:'))
+endereco = str(input('Endereco:'))
 
-iniciar.cadastroprodutos(categoria, produto, cor,
-                         precocompra, precovenda, estoque)
+iniciar.cadastrarusuario(usuario, email, tel, cpf, endereco)
